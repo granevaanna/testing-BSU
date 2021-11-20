07c58506f5ac940a6cd3793565134b18bc2536dc
Anna Graneva <granevaanna7@gmail.com>
2021-11-20 19:26:43 +0300

    update lab8

--- lab8/src/test/java/TestForExchanedButton.java
+++ lab8/src/test/java/TestForExchanedButton.java
@@ -1,5 +1,11 @@
-public class TestForExchanedButton {
-    public static void main(String[] args){
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
+public class TestForExchanedButton {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new SafariDriver();
+        driver.get("https://www.skyscanner.ru");
+        Thread.sleep(2000);
+        driver.quit();
     }
 }
