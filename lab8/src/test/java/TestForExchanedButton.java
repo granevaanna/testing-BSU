c4d6e35ea848035224944e1236420417303e23d2
Anna Graneva <granevaanna7@gmail.com>
2021-11-21 16:10:48 +0300

    add WebElements

--- lab8/src/test/java/TestForExchanedButton.java
+++ lab8/src/test/java/TestForExchanedButton.java
@@ -1,11 +1,25 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
+
+import javax.lang.model.element.Element;
 
 public class TestForExchanedButton {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new SafariDriver();
         driver.get("https://www.skyscanner.ru");
-        Thread.sleep(2000);
+
+        WebElement originSearch = driver.findElement(By.id("fsc-origin-search"));
+        originSearch.sendKeys("Минкс(Любой)");
+
+        WebElement destinationSearch = driver.findElement(By.id("fsc-destination-search"));
+        destinationSearch.sendKeys("Киев(Любой)");
+
+        WebElement exchangedButton = driver.findElement(By.xpath("//*[@id='flights-search-controls-root']"));
+        exchangedButton.click();
+
+        Thread.sleep(5000);
         driver.quit();
     }
 }
