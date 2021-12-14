27157a4d6bbc872572c5ec8431eac5e62c7e04db
Anna Graneva <granevaanna7@gmail.com>
2021-12-14 15:30:25 +0300

    add EskyStaysButtonTest

---
+++ lab9/src/main/java/EskyHomePage.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
+public class EskyHomePage {
+    private WebDriver driver;
+    private String HOMEPAGE_URL = "https://www.esky.com";
+
+    @FindBy(xpath = "//*[@id='multiQsf']//child::div[@class='mq-tab'][1]//child::a")
+    private WebElement hotelButton;
+
+    public EskyHomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public EskyHomePage openHomePage() {
+        driver.get(HOMEPAGE_URL);
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class);
+        return this;
+    }
+
+    public EskyHomePage openStaysPage(){
+        hotelButton.click();
+        return this;
+    }
+
+    public String getCurrentUrl(){
+        return driver.getCurrentUrl();
+    }
+}
---
+++ lab9/src/main/java/EskyStaysPage.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class EskyStaysPage {
+    private WebDriver driver;
+    private String STAYSPAGE_URL = "https://www.esky.com/stays/";
+
+    public EskyStaysPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public EskyStaysPage openHotelPage() {
+        driver.get(STAYSPAGE_URL);
+        return this;
+    }
+
+    public String getCurrentUrl(){
+        return driver.getCurrentUrl();
+    }
+}
